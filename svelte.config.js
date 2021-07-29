9ca7a5d5d8a5951d02b15ba122c63ae206f6e671
Tomek Rozalski <github@tomaszrozalski.pl>
2021-07-29 03:24:29 +0200

    [conf] testing build without 404

--- svelte.config.js
+++ svelte.config.js
@@ -14,9 +14,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
-			fallback: '404.html'
-		})
+		adapter: adapter()
 	}
 };
 
