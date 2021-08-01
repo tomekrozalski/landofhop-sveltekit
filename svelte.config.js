11e7b52add387f365562295823edd8bf4296187a
Tomek Rozalski <github@tomaszrozalski.pl>
2021-08-01 16:41:12 +0200

    [conf] Remove fallback from svelte-static, 404 page should work without that

--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,14 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
 		})
 	],
-
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
-			fallback: '404.html'
-		})
+		adapter: adapter()
 	}
 };
 
