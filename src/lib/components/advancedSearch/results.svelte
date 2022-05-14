80f4ead327dea49f5c261a5ede02a831183d86ed
Tomek Rozalski <github@tomaszrozalski.pl>
2022-05-14 22:04:50 +0200

    [advanced search] Create call to API and display fake data

--- src/lib/components/advancedSearch/results.svelte
+++ src/lib/components/advancedSearch/results.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+	import apiCall, { Endpoints } from '$lib/utils/api/call';
+	import type { Basics } from '$lib/utils/types/Beverage/Basics';
+	// import Spinner from '$lib/elements/spinner.svelte';
+	import BeverageList from '../beverageList/beverageList.svelte';
+
 	import type AdvancedSearchData from './AdvancedSearchData.type';
 
 	export let data: AdvancedSearchData;
+
+	async function callToApi(abc) {
+		console.log('abc', abc);
+
+		const response: Basics[] = await apiCall(fetch, Endpoints.searchByPhrase, {
+			pathParams: ['pl', 'ipa']
+		});
+
+		return response;
+	}
 
 	function updateData() {
 		const params = new URLSearchParams(location.search);
@@ -15,4 +30,12 @@
 
 <svelte:window on:popstate={updateData} />
 
-<div>{JSON.stringify(data)}</div>
+{#await callToApi(data)}
+	loading
+{:then beverages}
+	{#if beverages.length}
+		<BeverageList {beverages} />
+	{:else}
+		NothingFound
+	{/if}
+{/await}
