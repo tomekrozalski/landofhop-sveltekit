cc7280466a04b1dfad523211e3e19a8c842ea16c
Tomek Rozalski <github@tomaszrozalski.pl>
2022-05-22 16:46:53 +0200

    [beverageDetails] Start refactor cap images

--- src/lib/components/beverageDetails/gallery/cap.svelte
+++ src/lib/components/beverageDetails/gallery/cap.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import { translate } from 'svelte-intl';
 	import type { Details } from '$lib/utils/types/Beverage/Details';
 	import { PHOTO_SERVER } from '$lib/utils/constants';
 
 	export let details: Details;
 	const { badge, brand, photos, shortId } = details;
+	let isOpened = false;
 
 	const basicPath = `${PHOTO_SERVER}/${brand.badge}/${badge}/${shortId}`;
 	const pathJpgRegular = `${basicPath}/cap/jpg/1x.jpg`;
@@ -14,32 +16,37 @@
 </script>
 
 {#if photos?.cap}
-	<picture>
-		<source type="image/webp" srcSet="{pathWebpRegular} 1x, {pathWebpLarge} 2x" />
-		<source srcSet="{pathJpgRegular} 1x, {pathJpgLarge} 2x" />
+	{#if isOpened}
+		<button on:click={() => (isOpened = false)}>{$translate('beverage.cap.hide')}</button>
 		<img
 			alt={$translate('beverage.cap', { name: brand.name.value })}
-			srcset="{pathJpgRegular} 1x, {pathJpgLarge} 2x"
+			in:fade
+			srcset="{pathWebpRegular} 1x, {pathJpgLarge} 2x"
 			src={pathJpgRegular}
 			loading="lazy"
 		/>
-	</picture>
+	{:else}
+		<button on:click={() => (isOpened = true)}>{$translate('beverage.cap.show')}</button>
+	{/if}
 {/if}
 
 <style>
-	picture {
-		display: block;
-		width: 50%;
-		margin-top: 2rem;
-		opacity: 0.3;
-		transition: opacity var(--transition-default);
-	}
-
-	picture:hover {
-		opacity: 1;
-	}
-
-	picture img {
+	button {
 		width: 100%;
+		margin-top: 2rem;
+		background-color: var(--color-grey-5);
+		transition: background-color var(--transition-default);
+		font-size: 1.5rem;
+	}
+
+	button:hover {
+		background-color: var(--color-grey-4);
+	}
+
+	img {
+		display: block;
+		width: 100%;
+		margin-top: 1rem;
+		transition: opacity var(--transition-default);
 	}
 </style>
