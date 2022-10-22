fd9f437810bd8afcc9e7ca87617d855a143d28be
Tomek Rozalski <github@tomaszrozalski.pl>
2022-10-22 10:55:48 +0200

    [configuration] Types refactor. Fix last issues to complete build

--- src/lib/utils/api/index.ts
+++ src/lib/utils/api/index.ts
@@ -8,9 +8,16 @@ import deleteIfEmpty from './deleteIfEmpty';
 import formatBasics from './basics/format';
 import formatBeverageToBasics from './formatBeverageToBasics';
 import generateShortId from './generateShortId';
+import getTracedSvg from './photos/getTracedSvg';
 import translate from './translate';
 import updateSession from './sessions/update';
 import recalculateIngredientsOccurrences from './ingredients/recalculateOccurrences';
+import removeCover from './photos/removeCover';
+import removeGallery from './photos/removeGallery';
+import removeViewFromAbove from './photos/removeViewFromAbove';
+import { saveCoverJpg, saveCoverWebp } from './photos/saveCover';
+import { saveGalleryJpg, saveGalleryWebp } from './photos/saveGallery';
+import { saveViewFromAboveJpg, saveViewFromAboveWebp } from './photos/saveViewFromAbove';
 import updateRateBeerRating from './beverages/updateRateBeerRating';
 import updateUntappdRating from './beverages/updateUntappdRating';
 
@@ -25,8 +32,18 @@ export {
 	formatBeverageToBasics,
 	generateShortId,
 	generateTokens,
+	getTracedSvg,
 	recalculateIngredientsOccurrences,
+	removeCover,
+	removeGallery,
 	removeTokens,
+	removeViewFromAbove,
+	saveCoverJpg,
+	saveCoverWebp,
+	saveGalleryJpg,
+	saveGalleryWebp,
+	saveViewFromAboveJpg,
+	saveViewFromAboveWebp,
 	translate,
 	updateRateBeerRating,
 	updateSession,
