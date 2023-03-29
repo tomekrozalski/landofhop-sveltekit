3609eb49af1244a6c58a118d223584612e542577
Tomek Rozalski <github@tomaszrozalski.pl>
2023-03-29 21:14:47 +0200

    [brands] Fixes for mobile UI

--- src/routes/brands/Table/Header.svelte
+++ src/routes/brands/Table/Header.svelte
@@ -2,26 +2,35 @@
 	import { translate } from 'svelte-intl';
 </script>
 
-<ul class="header">
-	<li>{$translate('brands.header.name')}</li>
-	<li class="withSublist">
-		{$translate('brands.header.beveragesSummary')}
-		<ul>
-			<li>{$translate('brands.header.independently')}</li>
-			<li>{$translate('brands.header.asCooperator')}</li>
-			<li>{$translate('brands.header.asContractor')}</li>
-		</ul>
-	</li>
-	<li>{$translate('brands.header.averageRate')}</li>
-	<li>{$translate('brands.header.points')}</li>
-</ul>
+<div data-scrollsync>
+	<ul class="header">
+		<li>{$translate('brands.header.name')}</li>
+		<li class="withSublist">
+			{$translate('brands.header.beveragesSummary')}
+			<ul>
+				<li>{$translate('brands.header.independently')}</li>
+				<li>{$translate('brands.header.asCooperator')}</li>
+				<li>{$translate('brands.header.asContractor')}</li>
+			</ul>
+		</li>
+		<li>{$translate('brands.header.averageRate')}</li>
+		<li>{$translate('brands.header.points')}</li>
+	</ul>
+</div>
 
 <style>
+	div {
+		overflow-x: scroll;
+	}
+
 	ul.header {
+		min-width: 80rem;
+
 		display: grid;
 		grid-template-columns: 3fr 3fr 1fr 1fr;
 		border-bottom: 0.1rem solid var(--color-black);
 		background-color: var(--color-grey-5);
+
 		position: sticky;
 		top: var(--size-header-height);
 	}
--- src/routes/brands/Table/Table.svelte
+++ src/routes/brands/Table/Table.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import 'easy-scroll-sync';
 	import { page } from '$app/stores';
 	import MarkLanguage from '$lib/atoms/MarkLanguage.svelte';
 	import Header from './Header.svelte';
@@ -9,34 +10,43 @@
 
 <section>
 	<Header />
-	{#each insitutions as { badge, name, shortId, statsData }}
-		{@const { asContractor, asCooperator, avrScore, beverages, points } = statsData}
-		<a href="/brand/{shortId}/{badge}">
-			<div><MarkLanguage tag="span" {name} /></div>
-			<div class="independently">{beverages || '-'}</div>
-			<div>{asCooperator || '-'}</div>
-			<div class="contractor">{asContractor || '-'}</div>
-			<div>
-				{#if avrScore}
-					{new Intl.NumberFormat('pl', { maximumSignificantDigits: 3 }).format(avrScore.value)}
-				{:else}
-					-
-				{/if}
-			</div>
-			<div>
-				<Points value={points.value} />
-			</div>
-		</a>
-	{/each}
+	<div class="scrollsync" data-scrollsync>
+		{#each insitutions as { badge, name, shortId, statsData }}
+			{@const { asContractor, asCooperator, avrScore, beverages, points } = statsData}
+			<a href="/brand/{shortId}/{badge}">
+				<div><MarkLanguage tag="span" {name} /></div>
+				<div class="independently">{beverages || '-'}</div>
+				<div>{asCooperator || '-'}</div>
+				<div class="contractor">{asContractor || '-'}</div>
+				<div>
+					{#if avrScore}
+						{new Intl.NumberFormat('pl', { maximumSignificantDigits: 3 }).format(avrScore.value)}
+					{:else}
+						-
+					{/if}
+				</div>
+				<div>
+					<Points value={points.value} />
+				</div>
+			</a>
+		{/each}
+	</div>
 </section>
 
 <style>
 	section {
+		width: 100%;
 		max-width: var(--size-container-max-width);
 		margin: 0 auto 8rem auto;
 	}
 
+	.scrollsync {
+		overflow-x: scroll;
+	}
+
 	a {
+		min-width: 80rem;
+
 		display: grid;
 		grid-template-columns: 3fr 1fr 1fr 1fr 1fr 1fr;
 		border-bottom: 0.1rem solid var(--color-grey-3);
@@ -47,25 +57,31 @@
 		color: var(--color-white);
 	}
 
-	div {
+	a div {
 		display: flex;
 		align-items: center;
 		padding: 0.4rem 1rem;
 	}
 
-	div:not(:first-of-type) {
+	a div:not(:first-of-type) {
 		font-size: 1.5rem;
 	}
 
-	.independently {
+	a div.independently {
 		border-left: 0.1rem solid var(--color-grey-4);
 	}
 
-	.contractor {
+	a div.contractor {
 		border-right: 0.1rem solid var(--color-grey-4);
 	}
 
 	section :global(a) {
 		color: var(--color-black);
+	}
+
+	@media (min-width: 1024px) {
+		section {
+			overflow-x: unset;
+		}
 	}
 </style>
